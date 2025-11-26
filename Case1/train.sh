0192b44040b935c85ac7bd0a6b57a719864d8a2d
Christoph Lange <christoph.lange@tu-berlin.de>
2025-11-26 19:08:14 +0100

    trained_model for a long time

--- Case1/train.sh
+++ Case1/train.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
+#SBATCH --job-name=hp_tuning_bioprocessRL                                    # Job name
+#SBATCH --mail-type=ALL                                                 # Mail events (NONE, BEGIN, END, FAIL, ALL)
+#SBATCH --mail-user=christoph.lange@tu-berlin.de                        # Where to send mail.  Set this to your email address
+#SBATCH --ntasks=1                                                      # Number of MPI tasks (i.e. processes)
+#SBATCH --cpus-per-task=20                                               # Number of cores per MPI task 
+#SBATCH --gpus-per-task=0
+#SBATCH --nodes=1                                                       # Maximum number of nodes to be allocated
+#SBATCH --mem-per-cpu=3GB                                               # Memory (i.e. RAM) per processor
+#SBATCH --time=14-00:00:00                                              # Wall time limit (days-hrs:min:sec)
+#SBATCH --partition=standard                                                # standard cpu partition
+#SBATCH -D /scratch/ch.lange/git_projects/bioprocessRL/Case1		# Working Directory
+#SBATCH --account=kiwi
+
+set -x
+
+
 python Train_Case1.py \
-  --total-timesteps 2000000 \
+  --total-timesteps 1000000 \
   --n-parallel 20 \
   --learning-rate 3e-4 \
   --ent-coef 0.001 \
